1207f5032a2453e4aef44afc3ea0eb0fa7644069
PeterZaldivarGuarniz <108776625+PeterZaldivarGuarniz@users.noreply.github.com>
2022-07-29 17:42:00 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request, render_template
+import joblib
+import sklearn
+
+#curl -d "{\"Medidas\":[[14.13,19.29,91.97,654.89,0.1,0.1,0.09,0.05,0.18,0.06,0.41,1.22,2.87,40.34,0.01,0.03,0.03,0.01,0.02,0.0,16.27,25.68,107.26,880.58,0.13,0.25,0.27,0.11,0.29,0.08]]}" -H "Content-Type: applocation/json" -X POST http://127.0.0.1:5000/predecir
+#http get http://127.0.0.1:5000/predecir
+
+#curl "{\"Medidas\":[[14.13,19.29,91.97,654.89,0.1,0.1,0.09,0.05,0.18,0.06,0.41,1.22,2.87,40.34,0.01,0.03,0.03,0.01,0.02,0.0,16.27,25.68,107.26,880.58,0.13,0.25,0.27,0.11,0.29,0.08]]}" -H "Content-Type: applocation/json" -X POST http://127.0.0.1:5000/predecir
+
+app = Flask(__name__)
+
+def prec(x):
+    if round(x) == 0:
+        r = "Tumor benigno"
+    else:
+        r = "Tumor maligno"
+    return r
+
+@app.route("/")
+def home():
+    return 'hola mundo'
+
+@app.route("/predecir", methods=["POST"])
+def predecir():
+    try:
+        resultado = request.form
+        clf = joblib.load("modelo_arbol.pkl")
+        prediccion = clf.predict(resultado)
+        prediccion = prec(prediccion)
+    except:
+        prediccion = None
+    return render_template('http://127.0.0.1:8050', result={'Predicción': prediccion})
+
+if __name__ == '__main__':
+    app.run()
